84110d496320aa2a5af0f1124a9a7a14e90b1a75
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-18 04:06:57 +0000

    Add the layout of the main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-import { Navbar } from "./components";
+import { Navbar, Homepage, Cryptocurrencies, Exchanges, CryptoDetails, News } from "./components";
 
-import "./App.css"
+import "./App.css";
 
 const App = () => {
   return (
@@ -12,7 +12,15 @@ const App = () => {
         <div className="navbar">
           <Navbar />
         </div>
-        <div className="main"></div>
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/exchanges" element={<Exchanges />} />
+            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+            <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+        </div>
         <div className="footer"></div>
       </div>
     </>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.render(
-<BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />} />
-  </Routes>
-</BrowserRouter>, 
+import "antd/dist/antd.css";
 
-document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+
+  document.getElementById("root")
+);
