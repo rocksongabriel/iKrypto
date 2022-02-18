a21992311ce7d6ef06f3c4d278e377cf084c7d85
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-18 14:04:04 +0000

    Configure the redux store

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "./app/store";
 
 import "antd/dist/antd.css";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<App />} />
-    </Routes>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>,
 
   document.getElementById("root")
 );
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+// const options = {
+//   method: "GET",
+//   url: "https://coinranking1.p.rapidapi.com/coins",
+//   params: {
+//     referenceCurrencyUuid: "yhjMzLPhuIDl",
+//     timePeriod: "24h",
+//     tiers: "1",
+//     orderBy: "marketCap",
+//     orderDirection: "desc",
+//     limit: "50",
+//     offset: "0",
+//   },
+//   headers: {
+//     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+//     "x-rapidapi-key": "aacb4cf585msh54c3d3ca435065ap1dc64cjsn9c32a4957a59",
+//   },
+// };
