9ae8158a2824596560fe48a19b92c633953bfc8e
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-18 13:47:09 +0000

    Add structure to the Homepage component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Row, Col, Statistic } from "antd";
+
+const { Title } = Typography;
 
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <Title level={1} className="heading">Crypto Stats Around the Globe</Title>
+      <Row>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={5}/></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={5}/></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value={5}/></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={5}/></Col>
+        <Col span={12}><Statistic title="Total Markets" value={5}/></Col>
+      </Row>
+    </>
+  )
 }
 
 export default Homepage;
