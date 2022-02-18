08e06310915f7da4a116bea06f4c6ae8cbab52ac
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-18 03:43:27 +0000

    Add Navbar layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components";
+
+import "./App.css"
 
 const App = () => {
   return (
     <>
       <div className="app">
-        <div className="navbar"></div>
+        <div className="navbar">
+          <Navbar />
+        </div>
         <div className="main"></div>
         <div className="footer"></div>
       </div>
