299cc76f43b70d99d3509d491ffc7ba6442f9b95
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-18 15:33:28 +0000

    Add structure for list of news and cryptocurrencies on homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import { Typography, Row, Col, Statistic } from "antd";
 import millify from "millify";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Cryptocurrencies, News } from "./index";
 
 const { Title } = Typography;
 
@@ -16,26 +17,58 @@ function Homepage() {
 
   return (
     <>
-      <Title level={1} className="heading">
-        Crypto Stats Around the Globe
+      <Title level={6} className="heading">
+        Krypto Stats Around the Globe
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
+          <Statistic title="Total Kryptocurrencies" value={globalStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} />
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} />
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top Ten Kryptocurrencies In The World
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplefied />
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Krypto News In The World
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">show more</Link>
+        </Title>
+      </div>
+      <News simplefied />
     </>
   );
 }
