6de23a920a0aef3d2f6ada32c3b90f0ff6258137
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-18 14:55:33 +0000

    Fetch the stats and display it on the homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,22 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Typography, Row, Col, Statistic } from "antd";
+import millify from "millify";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Title } = Typography;
 
 function Homepage() {
+  const { data, isFetching } = useGetCryptosQuery();
+
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) return "Loading ...";
+
   return (
     <>
-      <Title level={1} className="heading">Crypto Stats Around the Globe</Title>
+      <Title level={1} className="heading">
+        Crypto Stats Around the Globe
+      </Title>
       <Row>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value={5}/></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value={5}/></Col>
-        <Col span={12}><Statistic title="Total Market Cap" value={5}/></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value={5}/></Col>
-        <Col span={12}><Statistic title="Total Markets" value={5}/></Col>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
+        </Col>
       </Row>
     </>
-  )
+  );
 }
 
 export default Homepage;
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,17 +1,22 @@
-// const options = {
-//   method: "GET",
-//   url: "https://coinranking1.p.rapidapi.com/coins",
-//   params: {
-//     referenceCurrencyUuid: "yhjMzLPhuIDl",
-//     timePeriod: "24h",
-//     tiers: "1",
-//     orderBy: "marketCap",
-//     orderDirection: "desc",
-//     limit: "50",
-//     offset: "0",
-//   },
-//   headers: {
-//     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-//     "x-rapidapi-key": "aacb4cf585msh54c3d3ca435065ap1dc64cjsn9c32a4957a59",
-//   },
-// };
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "aacb4cf585msh54c3d3ca435065ap1dc64cjsn9c32a4957a59",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
