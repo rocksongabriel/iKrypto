1164059ce64673441a9dc38ceff4e79ad506d83a
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-18 16:00:12 +0000

    Display the cryptocurrencies

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 function Cryptocurrencies() {
-  return <div>Cryptocurrencies</div>;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+
+  return (
+    <>
+      <Row gutter={[32, 32]}>
+        {cryptos.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
 
 export default Cryptocurrencies;
