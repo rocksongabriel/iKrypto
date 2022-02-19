8d5fe0393a10cc2e28dc2c7748110ac44f5fb7e3
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-02-19 02:01:46 +0000

    Fetch news and render them in the News component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import { Select, Row, Col, Card, Typography, Avatar } from "antd";
+import moment from "moment";
 
-function News() {
-  return <div>News</div>;
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const demoImage =
+  "https://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
+
+function News({ simplified }) {
+  const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({
+    newsCategory: "Cryptocurrency",
+    count: simplified ? 6 : 12,
+  });
+
+  if (isFetching) return "Loading ...";
+
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news) => (
+        <Col xs={24} sm={12} lg={12} key={news.datePublished}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="norefferer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+                <img
+                  src={news?.image?.thumbnail?.contentUrl || demoImage}
+                  alt={news.name}
+                />
+              </div>
+              <p>
+                {news.description > 100
+                  ? `${news.description.substring(0, 100)} ...`
+                  : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={
+                      news.provider[0]?.image?.thumbnail?.contentUrl ||
+                      demoImage
+                    }
+                  />
+                  <Text className="provider-name">{news.provider[0]?.name}</Text>
+                </div>
+                <Text>
+                  {moment(news.datePublished).startOf("ss").fromNow()}
+                </Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 }
 
 export default News;
